c34a9c8a86c8ab0b1068edd8d5916415001933ee
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-07-29 00:11:47 +0530

    app

---
+++ app/app.js
@@ -0,0 +1,95 @@
+import dotenv from "dotenv";
+import express from "express";
+import Stripe from "stripe";
+import dbConnect from "../config/dbConnect.js";
+import userRoutes from "../routes/usersRoute.js";
+import { globalErrHandler, notFound } from "../middlewares/globalErrHandler.js";
+import productsRouter from "../routes/productsRoute.js";
+import categoriesRouter from "../routes/categoriesRoute.js";
+import brandsRouter from "../routes/brandsRoute.js";
+import colorsRouter from "../routes/colorRoute.js";
+import reviewRouter from "../routes/reviewRoute.js";
+import orderRouter from "../routes/ordersRoute.js";
+import Order from "../model/Order.js";
+import couponsRouter from "../routes/couponsRoute.js";
+
+dotenv.config();
+
+// database connection
+dbConnect();
+
+const app = express();
+
+// stripe webhook
+// stripe instance
+const stripe = new Stripe(process.env.STRIPE_KEY);
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret = process.env.STRIPE_CLI_WEBHOOK_SECRET;
+
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  async (request, response) => {
+    const sig = request.headers["stripe-signature"];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    if (event.type === "checkout.session.completed") {
+      // update the order
+      const session = event.data.object;
+      const { orderId } = session.metadata;
+      const paymentStatus = session.payment_status;
+      const paymentMethod = session.payment_method_types[0];
+      const totalAmount = session.amount_total;
+      const currency = session.currency;
+
+      // find the order
+      const order = await Order.findByIdAndUpdate(
+        JSON.parse(orderId),
+        {
+          totalPrice: totalAmount / 100,
+          currency,
+          paymentMethod,
+          paymentStatus,
+        },
+        {
+          new: true,
+        }
+      );
+      console.log(order);
+    } else {
+      return;
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  }
+);
+
+// pass incoming data
+app.use(express.json());
+
+// routes
+app.use("/api/v1/users/", userRoutes);
+app.use("/api/v1/products/", productsRouter);
+app.use("/api/v1/categories/", categoriesRouter);
+app.use("/api/v1/brands/", brandsRouter);
+app.use("/api/v1/colors/", colorsRouter);
+app.use("/api/v1/reviews/", reviewRouter);
+app.use("/api/v1/orders/", orderRouter);
+app.use("/api/v1/coupons/", couponsRouter);
+
+// error middleware
+app.use(notFound);
+app.use(globalErrHandler);
+
+export default app;
