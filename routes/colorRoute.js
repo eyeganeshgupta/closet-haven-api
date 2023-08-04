a23ed387520270a841a93f0dc074e27595d5f355
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-08-04 19:02:52 +0530

    colorRoute

---
+++ routes/colorRoute.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { isLoggedIn } from "../middlewares/isLoggedIn.js";
+import {
+  createColorCtrl,
+  deleteColorCtrl,
+  getAllColorsCtrl,
+  getSingleColorCtrl,
+  updateColorCtrl,
+} from "../controllers/colorsCtrl.js";
+import isAdmin from "../middlewares/isAdmin.js";
+
+const colorsRouter = express.Router();
+
+colorsRouter.post("/", isLoggedIn, isAdmin, createColorCtrl);
+colorsRouter.get("/", getAllColorsCtrl);
+colorsRouter.get("/:id", getSingleColorCtrl);
+colorsRouter.put("/:id", isLoggedIn, isAdmin, updateColorCtrl);
+colorsRouter.delete("/:id", isLoggedIn, isAdmin, deleteColorCtrl);
+
+export default colorsRouter;
