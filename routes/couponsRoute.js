306334c656b4a2b67f695007fa8c63f70fb54c74
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-08-05 20:07:58 +0530

    couponsRoute

---
+++ routes/couponsRoute.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  createCouponCtrl,
+  deleteCouponCtrl,
+  getAllCouponsCtrl,
+  getCouponCtrl,
+  updateCouponCtrl,
+} from "../controllers/couponCtrl.js";
+import { isLoggedIn } from "../middlewares/isLoggedIn.js";
+import isAdmin from "../middlewares/isAdmin.js";
+
+const couponsRouter = express.Router();
+
+couponsRouter.post("/", isLoggedIn, isAdmin, createCouponCtrl);
+couponsRouter.get("/", getAllCouponsCtrl);
+couponsRouter.get("/:id", getCouponCtrl);
+couponsRouter.put("/update/:id", isLoggedIn, isAdmin, updateCouponCtrl);
+couponsRouter.delete("/delete/:id", isLoggedIn, isAdmin, deleteCouponCtrl);
+
+export default couponsRouter;
